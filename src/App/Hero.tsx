ee471388ead43adb80e14c24e514828569909095
Tung Teng <thetungctn@gmail.com>
2025-12-05 16:55:33 +0700

    Hero video

--- src/App/Hero.tsx
+++ src/App/Hero.tsx
@@ -1,10 +1,17 @@
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
+import { useMediaQuery } from 'react-responsive'
+
+import heroVideoSrc from '/assets/videos/hero.mp4'
+import smallVideoSrc from '/assets/videos/smallHero.mp4'
 
 const Hero = () => {
   useGSAP(() => {
     gsap.to('#hero', { opacity: 1, delay: 1.5 })
   })
+
+  const isMobile = useMediaQuery({ maxWidth: 767 })
+  const videoSrc = isMobile ? smallVideoSrc : heroVideoSrc
 
   return (
     <section className="w-full nav-height bg-black relative">
@@ -12,6 +19,17 @@ const Hero = () => {
         <p id="hero" className="hero-title">
           iPhone 15 Pro
         </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
       </div>
     </section>
   )
