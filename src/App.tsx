c541dab196324e25a897f61f8c9832b40f291f89
Tung Teng <thetungctn@gmail.com>
2025-12-05 11:06:29 +0700

    Body style

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="text-orange-500">Hello world</div>
-}
-
-export default App
---
+++ src/App/index.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <div>Navbar</div>
+      <div>Hero</div>
+      <div>Highlights</div>
+    </main>
+  )
+}
+
+export default App
