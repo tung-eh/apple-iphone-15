74749c23469c627a9cbe0c87bb0f332a17c57836
Tung Teng <thetungctn@gmail.com>
2025-12-05 11:23:15 +0700

    Animate hero title

--- src/App/Hero.tsx
+++ src/App/Hero.tsx
@@ -1,8 +1,17 @@
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+
 const Hero = () => {
+  useGSAP(() => {
+    gsap.to('#hero', { opacity: 1, delay: 1.5 })
+  })
+
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p id="hero">iPhone 15 Pro</p>
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
       </div>
     </section>
   )
