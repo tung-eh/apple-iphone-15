69e6fe09a4ebf7e734126760b2e2731af809c859
Tung Teng <thetungctn@gmail.com>
2025-12-06 14:04:42 +0700

    Highlights links

--- src/App/Highlights.tsx
+++ src/App/Highlights.tsx
@@ -1,9 +1,13 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 
+import watchImg from '/assets/images/watch.svg'
+import rightImg from '/assets/images/right.svg'
+
 const Highlights = () => {
   useGSAP(() => {
     gsap.to('#title', { opacity: 1, y: 0 })
+    gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 })
   })
 
   return (
@@ -16,6 +20,16 @@ const Highlights = () => {
           <h1 id="title" className="section-heading">
             Get the highlights.
           </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="right" className="ml-2" />
+            </p>
+          </div>
         </div>
       </div>
     </section>
