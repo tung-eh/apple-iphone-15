b688e85a20a94f8e2ce104beefa4c2f06c996f44
Tung Teng <thetungctn@gmail.com>
2025-12-09 10:54:35 +0700

    Apply chosen color to Iphone model

---
+++ src/App/Preview/constants.ts
@@ -0,0 +1,40 @@
+import yellowImg from '/assets/images/yellow.jpg'
+import blueImg from '/assets/images/blue.jpg'
+import whiteImg from '/assets/images/white.jpg'
+import blackImg from '/assets/images/black.jpg'
+
+export const models = [
+  {
+    id: 1,
+    title: 'iPhone 15 Pro in Natural Titanium',
+    color: ['#8F8A81', '#ffe7b9', '#6f6c64'],
+    img: yellowImg,
+  },
+  {
+    id: 2,
+    title: 'iPhone 15 Pro in Blue Titanium',
+    color: ['#53596E', '#6395ff', '#21242e'],
+    img: blueImg,
+  },
+  {
+    id: 3,
+    title: 'iPhone 15 Pro in White Titanium',
+    color: ['#C9C8C2', '#ffffff', '#C9C8C2'],
+    img: whiteImg,
+  },
+  {
+    id: 4,
+    title: 'iPhone 15 Pro in Black Titanium',
+    color: ['#454749', '#3b3b3b', '#181819'],
+    img: blackImg,
+  },
+]
+
+export type Model = (typeof models)[number]
+
+export const sizes = [
+  { label: '6.1"', value: 'small' },
+  { label: '6.7"', value: 'large' },
+]
+
+export type Size = (typeof sizes)[number]
--- src/App/Preview/index.tsx
+++ src/App/Preview/index.tsx
@@ -6,44 +6,8 @@ import { OrbitControls } from '@react-three/drei'
 import * as THREE from 'three'
 import { twMerge } from 'tailwind-merge'
 
-import yellowImg from '/assets/images/yellow.jpg'
-import blueImg from '/assets/images/blue.jpg'
-import whiteImg from '/assets/images/white.jpg'
-import blackImg from '/assets/images/black.jpg'
-
 import Iphone from './Iphone'
-
-const models = [
-  {
-    id: 1,
-    title: 'iPhone 15 Pro in Natural Titanium',
-    color: ['#8F8A81', '#ffe7b9', '#6f6c64'],
-    img: yellowImg,
-  },
-  {
-    id: 2,
-    title: 'iPhone 15 Pro in Blue Titanium',
-    color: ['#53596E', '#6395ff', '#21242e'],
-    img: blueImg,
-  },
-  {
-    id: 3,
-    title: 'iPhone 15 Pro in White Titanium',
-    color: ['#C9C8C2', '#ffffff', '#C9C8C2'],
-    img: whiteImg,
-  },
-  {
-    id: 4,
-    title: 'iPhone 15 Pro in Black Titanium',
-    color: ['#454749', '#3b3b3b', '#181819'],
-    img: blackImg,
-  },
-]
-
-const sizes = [
-  { label: '6.1"', value: 'small' },
-  { label: '6.7"', value: 'large' },
-]
+import { models, sizes, type Size } from './constants'
 
 const Preview = () => {
   useGSAP(() => {
@@ -53,7 +17,7 @@ const Preview = () => {
   const [modelIndex, setModelIndex] = useState(0)
   const model = models[modelIndex]
 
-  const [size, setSize] = useState<(typeof sizes)[number]['value']>('small')
+  const [size, setSize] = useState<Size['value']>('small')
 
   return (
     <section className="common-padding">
@@ -72,7 +36,7 @@ const Preview = () => {
                   rotateSpeed={0.4}
                   target={new THREE.Vector3(0, 0, 0)}
                 />
-                <Iphone scale={[17, 17, 17]} />
+                <Iphone scale={[17, 17, 17]} style={model} />
                 <ambientLight intensity={Math.PI / 2} />
                 <spotLight
                   position={[10, 10, 10]}
