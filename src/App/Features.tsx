10d7331b8c3d3c7ddf0c22355ee7c64cedef30a9
Tung Teng <thetungctn@gmail.com>
2025-12-10 01:43:58 +0700

    Features section content

--- src/App/Features.tsx
+++ src/App/Features.tsx
@@ -1,6 +1,10 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 
+import exploreVideo from '/assets/videos/explore.mp4'
+import explore1Img from '/assets/images/explore1.jpg'
+import explore2Img from '/assets/images/explore2.jpg'
+
 const Features = () => {
   const scrollProps = {
     toggleActions: 'restart reverse restart reverse',
@@ -33,6 +37,64 @@ const Features = () => {
             <h2 className="text-5xl lg:text-7xl font-semibold">
               Forged in titanium.
             </h2>
+          </div>
+          <div className="flex-center flex-col sm:px-10">
+            <div className="relative h-[50vh] w-full flex items-center">
+              <video
+                playsInline
+                id="exploreVideo"
+                className="w-full h-full object-cover object-center"
+                preload="none"
+                muted
+                autoPlay
+              >
+                <source src={exploreVideo} type="video/mp4" />
+              </video>
+            </div>
+
+            <div className="flex flex-col w-full relative">
+              <div className="feature-video-container">
+                <div className="overflow-hidden flex-1 h-[50vh]">
+                  <img
+                    src={explore1Img}
+                    alt="titanium"
+                    className="feature-video g_grow"
+                  />
+                </div>
+                <div className="overflow-hidden flex-1 h-[50vh]">
+                  <img
+                    src={explore2Img}
+                    alt="titanium 2"
+                    className="feature-video g_grow"
+                  />
+                </div>
+              </div>
+
+              <div className="feature-text-container">
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    iPhone 15 Pro is{' '}
+                    <span className="text-white">
+                      the first iPhone to feature an aerospace-grade titanium
+                      design
+                    </span>
+                    , using the same alloy that spacecrafts use for missions to
+                    Mars.
+                  </p>
+                </div>
+
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    Titanium has one of the best strength-to-weight ratios of
+                    any metal, making these our{' '}
+                    <span className="text-white">
+                      lightest Pro models ever.
+                    </span>
+                    You'll notice the difference the moment you pick one up.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
